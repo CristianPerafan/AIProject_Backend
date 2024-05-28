c0db24c51ee27e638360f896aa0470c27937344d
CristianPerafan <1110282139@u.icesi.edu.co>
2024-05-27 23:08:57 -0500

    Feat: update api

--- app/app.py
+++ app/app.py
@@ -1,10 +1,24 @@
 from fastapi import FastAPI, File, UploadFile
-
+from fastapi.middleware.cors import CORSMiddleware
 from model.model import __version__ as model_version
 from model.model import predict_pipeline
-
+from pydub import AudioSegment
+import os
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -16,4 +30,19 @@ def home():
 
 @app.post("/predict/")
 def predict(file: UploadFile = File(...)):
-    return predict_pipeline(file.file)
+
+
+    try:
+        with open(f"audio/{file.filename}", "wb") as f:
+            f.write(file.file.read())
+
+        prediction = predict_pipeline(f"audio/{file.filename}")
+
+        os.remove(f"audio/{file.filename}")
+
+        return prediction[0]
+    except:
+        return {
+            'message': 'An error occurred'
+        }
+    
--- app/model/model.py
+++ app/model/model.py
@@ -6,6 +6,11 @@ import pandas as pd
 import joblib
 
 
+"""
+    version: 0.1.0 - Includes unbalanced classes distribution
+"""
+
+
 __version__ = '0.1.0'
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent
@@ -21,10 +26,13 @@ with open(f"{BASE_DIR}/pca-{__version__}.pkl","rb") as f:
 
 RATE_HZ = 16000 # Audios are being sampled at 16,000 times per second
 
-def feature_extraction_with_means(file):
-    features = {}
-    audio,_ = librosa.load(file, sr=RATE_HZ)
+def feature_extraction_with_means(file_path):
 
+
+    features = {}
+
+    audio,_ = librosa.load(file_path) # Load the audio file (sr=None to keep the original sampling rate
+    
     spectral_centroid = np.mean(librosa.feature.spectral_centroid(y=audio, sr=RATE_HZ)) # Compute the spectral centroid
     spectral_bandwidth = np.mean(librosa.feature.spectral_bandwidth(y=audio, sr=RATE_HZ)) # Compute the spectral bandwidth
     spectral_rolloff = np.mean(librosa.feature.spectral_rolloff(y=audio, sr=RATE_HZ)) # Compute the spectral rolloff
@@ -37,16 +45,43 @@ def feature_extraction_with_means(file):
         features[f'mfcc_{i+1}'] = np.mean(el)
     
     return pd.DataFrame([features])
+    
+    
 
-def predict_pipeline(audio):
-    features = feature_extraction_with_means(audio)
 
+def predict_pipeline(file_path):
+    # Extraer características
+    features = feature_extraction_with_means(file_path)
+    
+    # Convertir DataFrame a numpy array si es necesario
     if isinstance(features, pd.DataFrame):
         features = features.to_numpy()
-    scaled_features = pca.transform(features)
-
-    scaled_features_df = pd.DataFrame(data = scaled_features, 
-                           columns = [f'pc_{i}' for i in range(1,16)])
     
-    return knn.predict(scaled_features_df)[0]
+    # Transformar características usando PCA
+    scaled_features = pca.transform(features)
+    
+    # Crear DataFrame con las características escaladas
+    scaled_features_df = pd.DataFrame(data=scaled_features, 
+                                      columns=[f'pc_{i}' for i in range(1, 16)])
+    
+    # Obtener las clases que el modelo puede predecir
+    class_labels = knn.classes_
+    print(class_labels)
+    
+    # Obtener las probabilidades de predicción
+    results = knn.predict_proba(scaled_features_df)
+
+    print(knn.predict(scaled_features_df))
+    
+    # Formar una lista de diccionarios con etiquetas y probabilidades
+    predictions = []
+    for sample_probs in results:
+        sample_prediction = []
+        for label, prob in zip(class_labels, sample_probs):
+            sample_prediction.append({'label': label, 'probability': prob})
+        predictions.append(sample_prediction)
+
+    
+    
+    return predictions
     
